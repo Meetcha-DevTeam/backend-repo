28f7120a2dc809d0022390a19dc4c0e67a25b77d
dasol <pds2023@gmail.com>
2025-07-01 13:10:01 +0900

    init: spring boot initialize

---
+++ src/main/java/com/meetcha/meetcha/MeetchaApplication.java
@@ -0,0 +1,13 @@
+package com.meetcha.meetcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetchaApplication.class, args);
+	}
+
+}
