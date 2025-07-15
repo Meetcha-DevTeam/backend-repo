7411d9ac64f982035c6c30c5526281add5cab70e
woo319 <132249641+woo319@users.noreply.github.com>
2025-07-15 12:33:27 +0900

    [FEAT] Security 임시 설정(모든 요청 허용)

--- src/main/java/Security/SecurityConfig.java
+++ src/main/java/Security/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                                .anyRequest().permitAll()  //개발할땐 우선 다 오픈!! 대신 배포 직전에 밑 코드로 수정필요
+                                .anyRequest().permitAll()  //개발할땐 우선 다 오픈!! 대신 배포 전에 밑 코드로 수정필요
                         //.requestMatchers("/oauth/google").permitAll() //로그인은 누구나
                         //.anyRequest().authenticated()                //나머지는 인증 필요
                 )
