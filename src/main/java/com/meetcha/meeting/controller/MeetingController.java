bc4ce3bcd101f65e94f2a42020faf4bf966009fb
woo319 <132249641+woo319@users.noreply.github.com>
2025-07-20 00:43:11 +0900

    [FEAT] global ApiResponse 생성

--- src/main/java/com/meetcha/meeting/controller/MeetingController.java
+++ src/main/java/com/meetcha/meeting/controller/MeetingController.java
@@ -1,11 +1,13 @@
 package com.meetcha.meeting.controller;
 
 import com.meetcha.auth.jwt.JwtProvider;
+import com.meetcha.global.dto.ApiResponse;
 import com.meetcha.global.exception.UnauthorizedException;
 import com.meetcha.meeting.dto.MeetingCreateRequest;
 import com.meetcha.meeting.dto.MeetingCreateResponse;
 import com.meetcha.meeting.service.MeetingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,15 +22,17 @@ public class MeetingController {
     private final JwtProvider jwtProvider;
 
     @PostMapping("/create")
-    public ResponseEntity<MeetingCreateResponse> createMeeting(
+    public ResponseEntity<ApiResponse<MeetingCreateResponse>> createMeeting(
             @RequestBody MeetingCreateRequest request,
             @RequestHeader("Authorization") String authHeader
-    ) {
+        ) {
         String token = extractToken(authHeader);
         UUID creatorBy = jwtProvider.getUserId(token);
         MeetingCreateResponse response = meetingService.createMeeting(request, creatorBy);
-        return ResponseEntity.status(201).body(response);
-    }
+        return ResponseEntity
+                .status(201)
+                .body(ApiResponse.success(201, "미팅이 성공적으로 생성되었습니다.", response));
+        }
 
     private String extractToken(String header) {
         if (header != null && header.startsWith("Bearer ")) {
