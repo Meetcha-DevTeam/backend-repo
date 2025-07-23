6fdda072b9dc510c4c4c99858895844397d8e169
dasol <pds2023@gmail.com>
2025-07-23 17:08:49 +0900

    feat: AlternativeTime,AlternativeVote Entity 구현

---
+++ src/main/java/com/meetcha/meetinglist/domain/AlternativeVoteEntity.java
@@ -0,0 +1,37 @@
+package com.meetcha.meetinglist.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "alternative_votes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class AlternativeVoteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "alternative_votes_id", nullable = false, updatable = false)
+    private UUID voteId;
+
+    @Column(name = "checked", nullable = false)
+    private boolean checked;
+
+    @Column(name = "alternative_times_id", nullable = false)
+    private UUID alternativeTimeId;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    public static AlternativeVoteEntity create(UUID alternativeTimeId, UUID userId) {
+        return AlternativeVoteEntity.builder()
+                .alternativeTimeId(alternativeTimeId)
+                .userId(userId)
+                .checked(true)
+                .build();
+    }
+}
