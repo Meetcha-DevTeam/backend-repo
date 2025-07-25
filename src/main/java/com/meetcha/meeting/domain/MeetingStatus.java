95d23257a3d82ee511f56b4d5244c6fb82f12c0a
dasol <pds2023@gmail.com>
2025-07-26 01:00:03 +0900

    chore: MeetingStatus MATCHING(매칭 중), MATCH_FAILED 추가

--- src/main/java/com/meetcha/meeting/domain/MeetingStatus.java
+++ src/main/java/com/meetcha/meeting/domain/MeetingStatus.java
@@ -1,6 +1,8 @@
 package com.meetcha.meeting.domain;
 
 public enum MeetingStatus {
+    MATCHING,       // 매칭 중
+    MATCH_FAILED,   // 매칭 실패
     BEFORE,  //미팅 시작 전
     ONGOING,  //미팅 진행 중
     DONE  //미팅 완료
