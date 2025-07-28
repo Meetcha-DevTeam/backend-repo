1858c391f6209fbd73187f8fa1b64b265d9384f2
dasol <pds2023@gmail.com>
2025-07-28 15:29:48 +0900

    feat: 미확정 미팅 자동 확정 스케줄러 구현

--- src/main/java/com/meetcha/meeting/domain/MeetingRepository.java
+++ src/main/java/com/meetcha/meeting/domain/MeetingRepository.java
@@ -2,6 +2,8 @@ package com.meetcha.meeting.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +12,10 @@ public interface MeetingRepository extends JpaRepository<MeetingEntity, UUID> {
 //    Optional<MeetingEntity> findByMeetingId(UUID meetingId);
 
     Optional<MeetingEntity> findByCode(String code);
+
+    List<MeetingEntity> findByMeetingStatusAndConfirmedTimeBefore(MeetingStatus meetingStatus, LocalDateTime now);
+
+    //상태가 MATCHING이고, 마감 시간이 지난 미팅을 모두 가져옵니다.
+    List<MeetingEntity> findByMeetingStatusAndDeadlineBefore(MeetingStatus status, LocalDateTime now);
+
 }
