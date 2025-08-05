f0864b408b29b5dbcfd29a55a9935129ccda8fbd
woo319 <132249641+woo319@users.noreply.github.com>
2025-08-05 13:57:28 +0900

    [FEAT] 미팅 회고 목록 요약 조회 구현

--- src/main/java/com/meetcha/reflection/dto/GetWrittenReflectionResponse.java
+++ src/main/java/com/meetcha/reflection/dto/GetWrittenReflectionResponse.java
@@ -9,7 +9,6 @@ import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class GetWrittenReflectionResponse {
     private UUID meetingId;
     private UUID projectId;        // nullable
@@ -18,4 +17,24 @@ public class GetWrittenReflectionResponse {
     private String confirmedTime;
     private String completedWork;  // nullable
     private String plannedWork;    // nullable
+
+    //JPQL에서 confirmedTime을 String으로 변환하므로, 생성자 명시 필요
+    public GetWrittenReflectionResponse(
+            UUID meetingId,
+            UUID projectId,
+            String projectName,
+            String title,
+            String confirmedTime,
+            String completedWork,
+            String plannedWork
+    ) {
+        this.meetingId = meetingId;
+        this.projectId = projectId;
+        this.projectName = projectName;
+        this.title = title;
+        this.confirmedTime = confirmedTime;
+        this.completedWork = completedWork;
+        this.plannedWork = plannedWork;
+    }
+
 }
