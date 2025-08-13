d4097c8d83acc6b989f770a10cba925e53a4b37d
woo319 <132249641+woo319@users.noreply.github.com>
2025-08-14 03:06:53 +0900

    [FEAT] 기여도+역할 요약 조회

--- src/main/java/com/meetcha/meeting/domain/MeetingRepository.java
+++ src/main/java/com/meetcha/meeting/domain/MeetingRepository.java
@@ -30,9 +30,21 @@ public interface MeetingRepository extends JpaRepository<MeetingEntity, UUID> {
     List<MeetingEntity> GetMeetingsNeedReflection(
             @Param("userId") UUID userId
     );
-    long countMeetingsNeedReflection(
-            @Param("userId") UUID userId
-    );
+
+    //기여도할일조회api에서 사용하는 쿼리
+    @Query("""
+    SELECT COUNT(m) FROM MeetingEntity m
+    WHERE m.createdBy = :userId
+       OR EXISTS (
+           SELECT 1 FROM ParticipantEntity p
+           WHERE p.meeting = m AND p.userId = :userId
+       )
+    AND NOT EXISTS (
+        SELECT 1 FROM MeetingReflectionEntity r
+        WHERE r.meeting = m AND r.user.userId = :userId
+    )
+""")
+    long countMeetingsNeedReflection(@Param("userId") UUID userId);
 
     // 투표 마감 후 가장 많이 투표된 대안 시간을 확정 시 사용
     @Query("""
@@ -61,5 +73,4 @@ public interface MeetingRepository extends JpaRepository<MeetingEntity, UUID> {
 """)
     List<MeetingEntity> findMyMeetings(@Param("userId") UUID userId);
 
-    //미작
 }
