d730aa59612d2e356acc6addace3e7cce1fab35d
dasol <pds2023@gmail.com>
2025-08-16 00:47:54 +0900

    feat: NeedReflectionResponse 내부에서 DateTimeUtils 적용

--- src/main/java/com/meetcha/meetinglist/dto/NeedReflectionResponse.java
+++ src/main/java/com/meetcha/meetinglist/dto/NeedReflectionResponse.java
@@ -1,17 +1,32 @@
 package com.meetcha.meetinglist.dto;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.meetcha.global.util.DateTimeUtils;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Builder
-public record NeedReflectionResponse(
-    UUID meetingId,
-    String title,
-    String description, // nullable
-    UUID projectId, //nullable
-    String projectName, //nullable
-    LocalDateTime confirmedTime,
-    String meetingStatus
-){}
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NeedReflectionResponse {
+    private UUID meetingId;
+    private String title;
+    private String description;
+    private UUID projectId;
+    private String projectName;
+
+    @JsonIgnore
+    private LocalDateTime confirmedTime; // UTC
+
+    private String meetingStatus;
+
+    @JsonProperty("confirmedTime")
+    public String getConfirmedTimeKst() {
+        return DateTimeUtils.utcToKstString(this.confirmedTime);
+    }
+}
