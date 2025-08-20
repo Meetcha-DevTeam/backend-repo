f799735871e207cdfff047fbd0b08749570022c3
woo319 <132249641+woo319@users.noreply.github.com>
2025-08-20 20:23:08 +0900

    [FEAT] 참여가능시간 조회

--- src/main/java/com/meetcha/joinmeeting/dto/GetSelectedTime.java
+++ src/main/java/com/meetcha/joinmeeting/dto/GetSelectedTime.java
@@ -2,7 +2,9 @@ package com.meetcha.joinmeeting.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class GetSelectedTime {
