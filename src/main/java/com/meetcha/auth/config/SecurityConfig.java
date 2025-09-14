0dbd3d6ee43149e2d22efeae3a28df3672036fc5
김민석 <skyfox001015@naver.com>
2025-09-15 02:31:43 +0900

    feat : 코스 수정

--- src/main/java/com/meetcha/auth/config/SecurityConfig.java
+++ src/main/java/com/meetcha/auth/config/SecurityConfig.java
@@ -34,17 +34,17 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
-        var config = new org.springframework.web.cors.CorsConfiguration();
-        config.setAllowedOriginPatterns(java.util.List.of("http://localhost:5173", "https://app.kuit5-meetcha.xyz")); // Vite 기본 포트
-        config.setAllowedMethods(java.util.List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
-        config.setAllowedHeaders(java.util.List.of("Authorization","Content-Type","Accept"));
-        config.setAllowCredentials(true); // 쿠키/세션이나 Authorization을 함께 쓰면 true
-
-        var source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
+//    @Bean
+//    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+//        var config = new org.springframework.web.cors.CorsConfiguration();
+//        config.setAllowedOriginPatterns(java.util.List.of("http://localhost:5173", "https://app.kuit5-meetcha.xyz")); // Vite 기본 포트
+//        config.setAllowedMethods(java.util.List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+//        config.setAllowedHeaders(java.util.List.of("Authorization","Content-Type","Accept"));
+//        config.setAllowCredentials(true); // 쿠키/세션이나 Authorization을 함께 쓰면 true
+//
+//        var source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
 
 }
