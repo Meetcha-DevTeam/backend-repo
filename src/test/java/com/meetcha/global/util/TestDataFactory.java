c4a10fbd3451df512f912a9620fbd03d3fe52e1b
JangIkhwan <bluensky0213@gmail.com>
2025-11-17 01:20:03 +0900

    test [KAN-175]: 테스트 데이터 생성 메서드 수정 및 추가
    - createMeeting 메서드에 미팅 생성 시간과 미팅 마감 시간을 인자로 추가
    - createMeetingParticipant 메서드 추가

--- src/test/java/com/meetcha/global/util/TestDataFactory.java
+++ src/test/java/com/meetcha/global/util/TestDataFactory.java
@@ -2,6 +2,8 @@ package com.meetcha.global.util;
 
 import com.meetcha.auth.domain.UserEntity;
 import com.meetcha.auth.domain.UserRepository;
+import com.meetcha.joinmeeting.domain.MeetingParticipant;
+import com.meetcha.joinmeeting.domain.MeetingParticipantRepository;
 import com.meetcha.meeting.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,23 +21,24 @@ public class TestDataFactory {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MeetingParticipantRepository meetingParticipantRepository;
+
     public UserEntity createUser(String email) {
         LocalDateTime now = LocalDateTime.now();
         UserEntity user = new UserEntity(null, "사용자1", email, "token", now, null, "refresh", now.plusDays(3));
         return userRepository.save(user);
     }
 
-    public MeetingEntity createMeeting(UUID userId) {
-        LocalDateTime now = LocalDateTime.now();
-
-        List<LocalDate> candDates = List.of(LocalDate.of(now.getYear(), now.getMonth(), now.getDayOfMonth() + 5), LocalDate.of(now.getYear(), now.getMonth(), now.getDayOfMonth() + 6));
+    public MeetingEntity createMeeting(UUID userId, LocalDateTime createdAt, LocalDateTime deadline) {
+        List<LocalDate> candDates = List.of(LocalDate.of(createdAt.getYear(), createdAt.getMonth(), createdAt.getDayOfMonth() + 5), LocalDate.of(createdAt.getYear(), createdAt.getMonth(), createdAt.getDayOfMonth() + 6));
 
         MeetingEntity meeting = MeetingEntity.builder()
                 .title("미팅1")
                 .description("미팅1입니다")
                 .durationMinutes(60)
-                .deadline(now.plusDays(2))
-                .createdAt(now)
+                .deadline(deadline)
+                .createdAt(createdAt)
                 .meetingStatus(MeetingStatus.MATCHING)
                 .confirmedTime(null)
                 .createdBy(userId)
@@ -49,5 +52,12 @@ public class TestDataFactory {
         meeting.setCandidateDates(candidateDates);
 
         return meetingRepository.save(meeting);
+    }
+
+    public MeetingParticipant createMeetingParticipant(String nickname, UUID userId, UUID meetingId) {
+        MeetingEntity meeting = meetingRepository.findById(meetingId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 미팅입니다."));
+        MeetingParticipant meetingParticipant = MeetingParticipant.create(userId, meeting, nickname);
+        return meetingParticipantRepository.save(meetingParticipant);
     }
 }
