7a671f4f37a48ce3c3bb0b6b4dc77bd677ddd5bb
JangIkhwan <bluensky0213@gmail.com>
2025-12-27 21:38:14 +0900

    feat [KAN-219]: MDC의 requestId의 값을 Nginx 변수로 변경
    - Nginx의 $request_id를 MDC에 저장하도록 수정

--- src/main/java/com/meetcha/log/filter/MDCFilter.java
+++ src/main/java/com/meetcha/log/filter/MDCFilter.java
@@ -2,24 +2,36 @@ package com.meetcha.log.filter;
 
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.jboss.logging.MDC;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class MDCFilter implements Filter {
+public class MDCFilter extends OncePerRequestFilter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         try {
-            MDC.put("requestId", UUID.randomUUID().toString());
+            String requestId = getRequestId(request);
+            MDC.put("requestId", requestId);
             chain.doFilter(request, response);
         } finally {
             MDC.clear();
         }
+    }
+
+    private static String getRequestId(HttpServletRequest request) {
+        String requestId = request.getHeader("X-Request-Id");
+        if(requestId == null || requestId.isEmpty()){
+            requestId = UUID.randomUUID().toString().replaceAll("-", "");
+        }
+        return requestId;
     }
 }
