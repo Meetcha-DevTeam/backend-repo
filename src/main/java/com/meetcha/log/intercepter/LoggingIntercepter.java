93ac877258147959f382030a04348af08fe0a4e7
JangIkhwan <bluensky0213@gmail.com>
2025-12-28 17:38:57 +0900

    feat [KAN-219]: 바디 내용을 API 요청 정보 로그에 추가
    - 바디 내용을 API 요청 정보 로그에 저장하도록 필터와 커스텀 래퍼 구현

--- src/main/java/com/meetcha/log/intercepter/LoggingIntercepter.java
+++ src/main/java/com/meetcha/log/intercepter/LoggingIntercepter.java
@@ -1,5 +1,6 @@
 package com.meetcha.log.intercepter;
 
+import com.meetcha.log.servlet.CustomHttpRequestWrapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +16,25 @@ import java.util.Map;
 public class LoggingIntercepter implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("API Request : clientIP = [{}] method = [{}] URL = [{}] params = [{}]", getClientIP(request), request.getMethod(), request.getRequestURL(), getRequestParams(request));
+        if (request.getParameterNames().hasMoreElements()){
+            log.info("API Request : clientIP = [{}] method = [{}] URL = [{}] params = [{}]", getClientIP(request), request.getMethod(), request.getRequestURL(), getRequestParams(request));
+        }
+        else{
+            log.info("API Request : clientIP = [{}] method = [{}] URL = [{}]", getClientIP(request), request.getMethod(), request.getRequestURL());
+        }
+
+        // request가 CustomHttpRequestWrapper로 래핑되어 있는지 확인
+        if(request instanceof CustomHttpRequestWrapper){
+            CustomHttpRequestWrapper requestWrapper = (CustomHttpRequestWrapper) request;
+            String requestBody = new String(requestWrapper.getRequestBody());
+
+            // Request Body가 있을 경우 로깅
+            if (!requestBody.isEmpty()) {
+                log.info("API Request : body = [{}]", requestBody);
+            }
+        }
+
+        // 요청 계속 진행
         return true;
     }
 
