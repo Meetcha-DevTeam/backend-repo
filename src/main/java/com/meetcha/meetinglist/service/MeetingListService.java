964c67221948d97e152178ff576b22559cedebd6
dasol <pds2023@gmail.com>
2026-01-08 23:54:27 +0900

    feat: 미팅 상세 조회 시 참여자 권한 검증 로직 추가

--- src/main/java/com/meetcha/meetinglist/service/MeetingListService.java
+++ src/main/java/com/meetcha/meetinglist/service/MeetingListService.java
@@ -34,11 +34,11 @@ public class MeetingListService {
         log.info("[MEETING_DETAIL] start meetingId={}", meetingId);
 
         //todo userID에 해당하는 meetingID 인지 검증하는 로직 필요할 듯
-//        boolean isParticipant = meetingParticipantRepository.existsByMeeting_MeetingIdAndUserId(meetingId, userId);
-//        if (!isParticipant) {
-//            log.warn("[MEETING_DETAIL] forbidden meetingId={} userId={}", meetingId, userId);
-//            throw new CustomException(ErrorCode.열람_권한이_없습니다);
-//        }
+        boolean isParticipant = meetingParticipantRepository.existsByMeeting_MeetingIdAndUserId(meetingId, userId);
+        if (!isParticipant) {
+            log.warn("[MEETING_DETAIL] forbidden meetingId={} userId={}", meetingId, userId);
+            throw new CustomException(ErrorCode.FORBIDDEN);
+        }
 
         MeetingEntity meeting = meetingRepository.findById(meetingId)
                 .orElseThrow(() -> {
